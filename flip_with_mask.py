7bba6d4c656692060fdfed897feea8a9774eef05
Rob Flynn <rjflynn2@sheffield.ac.uk>
2023-12-12 12:04:15 +0000

    changed for version with 3 dimensions

--- flip_with_mask.py
+++ flip_with_mask.py
@@ -1,13 +1,17 @@
 import torch
 
 def flip_with_mask(x, lengths, mask=None): # flips tensor but keeps masked values at the end of the sequence
-    mask = ~(torch.arange(x.size(1)).expand(x.size()) < lengths.unsqueeze(1)) if mask is None else mask
-    selection_indices = torch.arange(x.size(1) - 1, -1, -1)[None].expand(x.size())
+    # x: (batch, seq_len, d)
+    # lengths: (batch,)
+    # mask: (batch, seq_len)
     max_len = lengths.max()
-    offset = (max_len - lengths)[:, None].expand(x.size()) # offset based on difference from max length
+    mask = ~(torch.arange(max_len, device=lengths.device) < lengths.unsqueeze(1)) if mask is None else mask
+    selection_indices = torch.arange(x.size(1) - 1, -1, -1)[None].expand(x.size()[:-1])
+    max_len = lengths.max()
+    offset = (max_len - lengths)[:, None].expand(x.size()[:-1]) # offset based on difference from max length
     selection_indices = selection_indices - offset # take offset from indices
     selection_indices[mask] = max_len - 1 # set values that are out of range to the last index
-    x = x.gather(1, selection_indices)
+    x = x.gather(1, selection_indices.unsqueeze(-1).expand(x.size()))
     return x
 
 
@@ -17,8 +21,12 @@ if __name__ == '__main__':
     x = torch.arange(12)[None].repeat(4, 1) + 1
     mask = ~(torch.arange(x.size(1)).expand(x.size()) < lengths.unsqueeze(1))
     x.masked_fill_(mask, 0)
+    # add dim
+    d = 256
+    x = x[..., None].repeat(1, 1, d)
+    print(x.shape)
     f = flip_with_mask(x, lengths)
     print('before:')
-    print(x)
+    print(x[..., 0])
     print('after:')
-    print(f)
+    print(f[..., 0])
